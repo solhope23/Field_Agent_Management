4224ca4c6204a707e79a36b4ff22399d1ab00187
solhope23 <s526413103@gmail.com>
2025-06-08 15:54:45 +0300

    סיום קונסטרקטור

---
+++ FieldAgentManagement/AgentDAL.cs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+public class AgentDAL
+{
+    private string connStr;
+    private MySqlConnection agents;
+
+    public AgentDAL(string server, string username, string password = "", string database)
+    {
+        connStr = $"server={server};username={username};password={password};database={database}";
+
+        this.agents = new MySqlConnection(connStr);
+
+        TryConnect(); 
+    }
+
+    private void TryConnect()
+    {
+        try
+        {
+            this.agents.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"err: {e.Message}");
+        }
+        finally
+        {
+            this.agents.Close();
+        }
+    }
+
+    public 
+
+}
